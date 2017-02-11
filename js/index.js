26799f1376968c449b87cc5bc2a3ba25c360e837
nidage <454618448@qq.com>
2017-02-11 22:09:47 +0800

    some

---
+++ js/index.js
@@ -0,0 +1,73 @@
+﻿$(function(){
+  $('#dowebok').fullpage({
+    sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90'],
+    'navigation': true,
+    afterLoad: function(anchorLink, index){
+      if(index == 2){
+        $('.section2').find('p').delay(500).animate({
+          left: '0'
+        }, 1500, 'easeOutExpo');
+        $('.section2').find('h2').css("border-bottom","2px solid red");
+        $('.section2').find('h2').css("margin","-10px 620px 0 620px");
+        $(".ani").fadeIn(1000, "linear");
+        $(".ani").html("·About me·");
+
+
+      }
+      if(index == 3){
+        $('.section3').find('p').delay(500).animate({
+          bottom: '0'
+        }, 1500, 'easeOutExpo');
+      }
+      if(index == 4){
+        $('.section4').find('p').fadeIn(2000);
+      }
+    },
+    onLeave: function(index, direction){
+      if(index == '2'){
+        $('.section2').find('p').delay(500).animate({
+          left: '-120%'
+        }, 1500, 'easeOutExpo');
+        $(".ani").fadeOut(1000, "linear");
+        $('.section2').find('h2').css("border-bottom","2px solid transparent");
+      }
+      if(index == '3'){
+        $('.section3').find('p').delay(500).animate({
+          bottom: '-120%'
+        }, 1500, 'easeOutExpo');
+      }
+      if(index == '4'){
+        $('.section4').find('p').fadeOut(2000);
+      }
+    }
+  });
+});
+
+$("#header_p").hover(
+        function() {
+            $("#header_p1").html("Resume");
+        },
+        function() {
+            $("#header_p1").html("谭愿波");
+        } 
+    );
+
+$("#header_p").hover(
+        function() {
+            $("#header_p2").html("前端工程师");
+        },
+        function() {
+            $("#header_p2").html("个人简历");
+        } 
+    );
+
+$("#home_head_content").hover(
+        function() {
+            $("#home_photo1").css("display","none");
+            $("#home_photo2").css("display","inline-block");
+        },
+        function() {
+            $("#home_photo2").css("display","none");
+            $("#home_photo1").css("display","inline-block");
+        } 
+    );
